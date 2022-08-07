f9c8767e65d7a519ff5afcfcab4f9a0679810190
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-08-07 17:01:52 +0300

    Произведение элементов матрицы на аналогичных позициях

---
+++ 58/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте две матрицы.
+// Напишите программу, которая будет находить произведение двух матриц.
+
+void FillArrayNew(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($" {arr[i, j] = new Random().Next(1, 9)}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultiplexerArray(int[,] arr1, int[,] arr2, int[,] newArray)
+{
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            {
+                newArray[i, j] += arr1[i, j] * arr2[i, j];
+            }
+        }
+    }
+}
+
+int[,] array1 = new int[3, 3];
+int[,] array2 = new int[3, 3];
+int[,] resultArray = new int[3, 3];
+Console.WriteLine("Первая матрица:");
+FillArrayNew(array1);
+Console.WriteLine("Вторая матрица:");
+FillArrayNew(array2);
+Console.WriteLine("Произведение матриц:");
+MultiplexerArray(array1, array2, resultArray);
+PrintArray(resultArray);
